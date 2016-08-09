501d4a66084f57a9b163cca7a8fb4596cd4df1a9
Michał Wełniak <Micha Weniak>
2016-08-09 19:39:11 +0200

    js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,43 @@
-$(document).foundation();
+// $(document).foundation();
+
+$(document).ready(function(){
+			$(".buttonlog button").click(function() { 
+				var haslo= $(".haslo #user_login").val();
+				
+				if (haslo.length != 0){ 
+
+						sendlog (haslo); 
+					}
+					else{
+						alert("Hasło jest puste");
+				}
+			});
+			// $(".avatarlog").click(function() { 
+				
+			//	var avatar= $(".avatarlog img").attr("src");
+			//	alert (avatar); 
+			//});
+			$(".avatarlog").click(function() { 
+				
+				$(".avatarlog img").attr("src" ,"https://cdn3.iconfinder.com/data/icons/user-avatars-1/512/users-10-3-128.png");
+			
+			});
+			function sendlog(haslo){
+				$.ajax({
+					  type: "post",
+					  data: {
+					    login: "efi",
+					    password: haslo
+					  },
+					  url: "https://efigence-camp.herokuapp.com/api/login",
+					  error: function(response) {
+					    console.log(response.responseText);
+					    alert("Złe hasło");
+					  },
+					  success: function(response) {
+					    console.log(response);
+					  }
+				});
+			}
+
+});
